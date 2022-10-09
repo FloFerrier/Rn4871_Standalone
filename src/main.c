55e636d8b1cb05303bce82f88305528771cd89c8
flfer <florian.ferrier@tutamail.com>
2022-10-09 11:46:37 +0200

    Update project

--- src/main.c
+++ src/main.c
@@ -17,47 +17,43 @@
 int serialPort;
 FILE *log_file;
 
-uint8_t rn4871UartTxAPI(char *pBuffer, uint16_t *bufferSize);
-uint8_t rn4871UartRxAPI(char *pBuffer, uint16_t *bufferSize);
-void rn4871DelayMsAPI(uint32_t delay);
-void rn4871LogSenderAPI(char *log, int logLen);
+uint8_t rn4871UartTxCb(char *pBuffer, uint16_t *bufferSize);
+uint8_t rn4871UartRxCb(char *pBuffer, uint16_t *bufferSize);
+void rn4871DelayMsCb(uint32_t delay);
+void rn4871LogSender(char *log, int logLen);
 
-void rn4871LogSenderAPI(char *log, int logLen) {
-	if(NULL != log_file) {
+void rn4871LogSender(char *log, int logLen)
+{
+	if(NULL != log_file)
+	{
 		fprintf(log_file, "%s", log);
 	}
 }
 
-uint8_t rn4871UartTxAPI(char *pBuffer, uint16_t *bufferSize) {
+uint8_t rn4871UartTxCb(char *pBuffer, uint16_t *bufferSize)
+{
     assert((NULL != pBuffer) || (NULL != bufferSize));
 	//printf("[TX:%d] %s\r\n", *bufferSize, pBuffer);
 
-	if(VIRTUAL_MODULE) {
-		virtualModuleReceiveData((char*)pBuffer, *bufferSize);
-	}
-	else {
-		ssize_t sizeWrite = write(serialPort, pBuffer, *bufferSize);
-		if(0 >= sizeWrite) {
-			printf("Fail to send data : [%d] %s\r\n", *bufferSize, pBuffer);
-			return CODE_RETURN_UART_FAIL;
-		}
+	ssize_t sizeWrite = write(serialPort, pBuffer, *bufferSize);
+	if(0 >= sizeWrite)
+	{
+		//printf("Fail to send data : [%d] %s\r\n", *bufferSize, pBuffer);
+		return CODE_RETURN_UART_FAIL;
 	}
 
     return CODE_RETURN_SUCCESS;
 }
 
-uint8_t rn4871UartRxAPI(char *pBuffer, uint16_t *bufferSize) {
+uint8_t rn4871UartRxCb(char *pBuffer, uint16_t *bufferSize)
+{
     assert((NULL != pBuffer) || (NULL != bufferSize));
 
 	memset(pBuffer, '\0', BUFFER_UART_MAX_LEN);
-	if(VIRTUAL_MODULE) {
-		virtualModuleSendData((char*)pBuffer, bufferSize);
-	}
-	else {
-		*bufferSize = read(serialPort, pBuffer, BUFFER_UART_MAX_LEN);
-	}
+	*bufferSize = read(serialPort, pBuffer, BUFFER_UART_MAX_LEN);
 
-	if (0 >= *bufferSize) {
+	if (0 >= *bufferSize)
+	{
 		//printf("Fail to receive data: bufferSize=%d\r\n", *bufferSize);
 		return CODE_RETURN_UART_FAIL;
   	}
@@ -66,208 +62,180 @@ uint8_t rn4871UartRxAPI(char *pBuffer, uint16_t *bufferSize) {
     return CODE_RETURN_SUCCESS;
 }
 
-void rn4871DelayMsAPI(uint32_t delay) {
+void rn4871DelayMsCb(uint32_t delay)
+{
 	usleep(delay*1000);
 }
 
-int main (void) {
-
+int main (void)
+{
+	printf("---- RN4871 Standalone ----\r\n");
 	printf("Logs are stored on the file : %s\r\n", LOG_FILE_NAME);
-	log_file = fopen(LOG_FILE_NAME, "w");
-	if(NULL == log_file) {
+	//log_file = fopen(LOG_FILE_NAME, "w");
+	/*if(NULL == log_file)
+	{
 		printf("Open log file fail ...\r\n");
+	}*/
+
+	printf("Serial port configuration\r\n");
+	serialPort = open(FILENAME_SERIAL_PORT, O_RDWR);
+	if(0 >= serialPort)
+	{
+		printf("Fail to open serial port ...\r\n");
+		//fclose(log_file);
+		return -1;
 	}
 
-	if(VIRTUAL_MODULE) {
-		printf("Virtual module selected !\r\n");
-	}
-	else {
-		printf("Real module selected !\r\n");
-		printf("Serial port configuration\r\n");
-		serialPort = open(FILENAME_SERIAL_PORT, O_RDWR);
-		if(0 >= serialPort) {
-			printf("Fail to open serial port ...\r\n");
-			fclose(log_file);
-			return -1;
-		}
-		printf("Serial port open !\r\n");
-		struct termios tty;
-		if(0 != tcgetattr(serialPort, &tty)) {
-			printf("Error %i from tcgetattr: %s\n", errno, strerror(errno));
-			fclose(log_file);
-			return -1;
-		}
-
-		printf("Get serial configuration with success !\r\n");
-
-		tty.c_cflag &= ~PARENB; // Clear parity bit, disabling parity (most common)
-		tty.c_cflag &= ~CSTOPB; // Clear stop field, only one stop bit used in communication (most common)
-		tty.c_cflag &= ~CSIZE; // Clear all bits that set the data size 
-		tty.c_cflag |= CS8; // 8 bits per byte (most common)
-		tty.c_cflag &= ~CRTSCTS; // Disable RTS/CTS hardware flow control (most common)
-		tty.c_cflag |= CREAD | CLOCAL; // Turn on READ & ignore ctrl lines (CLOCAL = 1)
-
-		tty.c_lflag &= ~ICANON;
-		tty.c_lflag &= ~ECHO; // Disable echo
-		tty.c_lflag &= ~ECHOE; // Disable erasure
-		tty.c_lflag &= ~ECHONL; // Disable new-line echo
-		tty.c_lflag &= ~ISIG; // Disable interpretation of INTR, QUIT and SUSP
-		tty.c_iflag &= ~(IXON | IXOFF | IXANY); // Turn off s/w flow ctrl
-		tty.c_iflag &= ~(IGNBRK|BRKINT|PARMRK|ISTRIP|INLCR|IGNCR|ICRNL); // Disable any special handling of received bytes
-
-		tty.c_oflag &= ~OPOST; // Prevent special interpretation of output bytes (e.g. newline chars)
-		tty.c_oflag &= ~ONLCR; // Prevent conversion of newline to carriage return/line feed
-		// tty.c_oflag &= ~OXTABS; // Prevent conversion of tabs to spaces (NOT PRESENT ON LINUX)
-		// tty.c_oflag &= ~ONOEOT; // Prevent removal of C-d chars (0x004) in output (NOT PRESENT ON LINUX)
-
-		tty.c_cc[VTIME] = 10; // Wait for up to 1s (10 deciseconds), returning as soon as any data is received.
-		tty.c_cc[VMIN] = 0;
-
-		// Set baudrate at 115200 bauds
-		cfsetispeed(&tty, B115200);
-		cfsetospeed(&tty, B115200);
-
-		if(0 != tcsetattr(serialPort, TCSANOW, &tty)) {
-			printf("Error %i from tcsetattr: %s\r\n", errno, strerror(errno));
-			fclose(log_file);
-			return -1;
-		}
-
-		printf("Set new serial configuration with success !\r\n");
+	printf("Serial port open !\r\n");
+	struct termios tty;
+	if(0 != tcgetattr(serialPort, &tty))
+	{
+		printf("Error %i from tcgetattr: %s\n", errno, strerror(errno));
+		//fclose(log_file);
+		return -1;
 	}
 
-	/* RN4871 Usecase */
-	struct rn4871_dev_s dev = {
-		.uartTx = rn4871UartTxAPI,
-		.uartRx = rn4871UartRxAPI,
-		.delayMs = rn4871DelayMsAPI,
+	printf("Get serial configuration with success !\r\n");
+
+	tty.c_cflag &= ~PARENB;
+	tty.c_cflag &= ~CSTOPB;
+	tty.c_cflag &= ~CSIZE;
+	tty.c_cflag |= CS8;
+	tty.c_cflag &= ~CRTSCTS;
+	tty.c_cflag |= CREAD | CLOCAL;
+	tty.c_lflag &= ~ICANON;
+	tty.c_lflag &= ~ECHO;
+	tty.c_lflag &= ~ECHOE;
+	tty.c_lflag &= ~ECHONL;
+	tty.c_lflag &= ~ISIG;
+	tty.c_iflag &= ~(IXON | IXOFF | IXANY);
+	tty.c_iflag &= ~(IGNBRK|BRKINT|PARMRK|ISTRIP|INLCR|IGNCR|ICRNL);
+	tty.c_oflag &= ~OPOST;
+	tty.c_oflag &= ~ONLCR;
+	tty.c_cc[VTIME] = 10;
+	tty.c_cc[VMIN] = 0;
+	cfsetispeed(&tty, B115200);
+	cfsetospeed(&tty, B115200);
+
+	if(0 != tcsetattr(serialPort, TCSANOW, &tty))
+	{
+		printf("Error %i from tcsetattr: %s\r\n", errno, strerror(errno));
+		//fclose(log_file);
+		return -1;
+	}
+
+	printf("Set new serial configuration with success !\r\n");
+
+	struct rn4871_conf_s config =
+	{
+		.deviceName = "RN4871-standalone",
+		.services = DEVICE_INFORMATION | UART_TRANSPARENT,
 	};
 
-	uint8_t ret = rn4871EnterCommandMode(&dev);
-	if(CODE_RETURN_SUCCESS != ret) {
-		printf("Fail to enter on command mode ...\r\n");
+	struct rn4871_dev_s module =
+	{
+		.uartTx = rn4871UartTxCb,
+		.uartRx = rn4871UartRxCb,
+		.delayMs = rn4871DelayMsCb,
+	};
+
+	uint8_t ret = rn4871Init(&module);
+	if(CODE_RETURN_SUCCESS != ret)
+	{
+		printf("Fail to enter on command mode ...[%s]\r\n", rn4871GetErrorCodeStr(ret));
 		close(serialPort);
-		fclose(log_file);
+		//fclose(log_file);
+		return 0;
+	}
+
+	ret = rn4871EnterCommandMode(&module);
+	if(CODE_RETURN_SUCCESS != ret)
+	{
+		printf("Fail to enter on command mode ...[%s]\r\n", rn4871GetErrorCodeStr(ret));
+		close(serialPort);
+		//fclose(log_file);
 		return 0;
 	}
 	printf("RN4871 is on command mode\r\n");
 
-	const char deviceName[21] = "test_main";
-	ret = rn4871SetDeviceName(&dev, deviceName, strlen(deviceName));
-	if(CODE_RETURN_SUCCESS != ret) {
-		printf("Fail to set device name ...%d\r\n", ret);
+	ret = rn4871SetConfig(&module, &config);
+	if(CODE_RETURN_SUCCESS != ret)
+	{
+		printf("Fail to set config ...[%s]\r\n", rn4871GetErrorCodeStr(ret));
 	}
 
-	char realDeviceName[21] = "";
-	ret = rn4871GetDeviceName(&dev, realDeviceName);
-	if(CODE_RETURN_SUCCESS != ret) {
-		printf("Fail to get device name ...%d\r\n", ret);
+	ret = rn4871RebootModule(&module);
+	if(CODE_RETURN_SUCCESS != ret)
+	{
+		printf("Fail to reboot module ...[%s]\r\n", rn4871GetErrorCodeStr(ret));
+		close(serialPort);
+		return 0;
 	}
+	printf("RN4871 is rebooting !\r\n");
 
-	if(0 == strcmp(deviceName, realDeviceName)) {
-		printf("RN4871 module is now name : %s\r\n", realDeviceName);
-	}
-	else {
-		printf("New device name is not setting correctly ...\r\n");
-	}
-
-	char firmwareVersion[BUFFER_LEN_MAX+1] = "";
-	ret = rn4871GetFirmwareVersion(&dev, firmwareVersion);
-	if(CODE_RETURN_SUCCESS != ret) {
-		printf("Fail to get firmware version ...%d\r\n", ret);
-	}
-	else {
-		printf("RN4871 module firmware version : %s\r\n", firmwareVersion);
-	}
-
-	char macAddress[BUFFER_LEN_MAX+1] = "";
-	ret = rn4871GetMacAddress(&dev, macAddress);
-	if(CODE_RETURN_SUCCESS != ret) {
-		printf("Fail to get mac address ...%d\r\n", ret);
-	}
-	else {
-		printf("RN4871 module mac address : %s\r\n", macAddress);
-	}
-
-	ret = rn4871SetServices(&dev, DEVICE_INFORMATION | UART_TRANSPARENT);
-	if(CODE_RETURN_SUCCESS != ret) {
-		printf("Fail to set services ...%d\r\n", ret);
-	}
-	else {
-		printf("RN4871 module services as Transparent UART\r\n");
-	}
-
-	ret = rn4871RebootModule(&dev);
-	if(CODE_RETURN_SUCCESS != ret) {
-		printf("Fail to reboot module ...%d\r\n", ret);
-	}
-	printf("RN4871 module reboot with success\r\n");
-
-	ret = rn4871EnterCommandMode(&dev);
-	if(CODE_RETURN_SUCCESS != ret) {
-		printf("Fail to enter on command mode ...\r\n");
-		usleep(100);
-		ret = rn4871EnterCommandMode(&dev);
-		if(CODE_RETURN_SUCCESS != ret) {
-			printf("Fail to enter on command mode ...\r\n");
+	ret = rn4871EnterCommandMode(&module);
+	if(CODE_RETURN_SUCCESS != ret)
+	{
+		printf("Fail to enter on command mode ...[%s]\r\n", rn4871GetErrorCodeStr(ret));
+		/* Workaround with a retry ... */
+		ret = rn4871EnterCommandMode(&module);
+		if(CODE_RETURN_SUCCESS != ret)
+		{
+			printf("Fail to enter on command mode ...[%s]\r\n", rn4871GetErrorCodeStr(ret));
 			close(serialPort);
-			fclose(log_file);
 			return 0;
 		}
 	}
 	printf("RN4871 is on command mode\r\n");
 
-	bool modeIsTransparentUart = false;
-	ret = rn4871IsOnTransparentUart(&dev, &modeIsTransparentUart);
-	if(CODE_RETURN_SUCCESS != ret) {
-		printf("Fail on rn4871IsOnTransparentUart function ...\r\n");
-		close(serialPort);
-		fclose(log_file);
-		return 0;
+	ret = rn4871GetConfig(&module, &config);
+	if(CODE_RETURN_SUCCESS != ret)
+	{
+		printf("Fail to get config ...[%s]\r\n", rn4871GetErrorCodeStr(ret));
 	}
-	if(!modeIsTransparentUart) {
-		printf("RN4871 module is not on Transparent Uart mode ...\r\n");
-		close(serialPort);
-		fclose(log_file);
-		return 0;
-	}
-	ret = rn4871QuitCommandMode(&dev);
-	if(CODE_RETURN_SUCCESS != ret) {
-		printf("Fail to quit on command mode ...\r\n");
-		close(serialPort);
-		fclose(log_file);
-		return 0;
+	else
+	{
+		printf("------ Configuration ------\r\n");
+		printf("Device name: %s\r\n", config.deviceName);
+		printf("Firmware version: %s\r\n", config.firmwareVersion);
+		printf("Mac address: %s\r\n", config.macAddress);
+		printf("Services: 0x%02X\r\n", config.services);
+		printf("---------------------------\r\n");
 	}
 
-	/* Wait connection and streaming by an external device */
-	char *dataToRecv = malloc(sizeof(char)*(BUFFER_LEN_MAX+1));
-	uint16_t dataToRecvLen = 0;
-	while(FSM_STATE_STREAMING != rn4871GetFsmState()) {
-		rn4871WaitReceivedData(&dev, dataToRecv, &dataToRecvLen);
-		printf("[%d] %s\r\n", dataToRecvLen, dataToRecv);
-		/* TODO
-		if(VIRTUAL_MODULE) {
-			virtualModuleConnect(&dev);
-			virtualModuleStream(&dev);
-		}*/
+	ret = rn4871QuitCommandMode(&module);
+	if(CODE_RETURN_SUCCESS != ret)
+	{
+		printf("Fail to quit command mode ...[%s]\r\n", rn4871GetErrorCodeStr(ret));
+		close(serialPort);
+		return 0;
 	}
-	printf("DEBUG FSM state : %d\r\n", rn4871GetFsmState());
+	printf("Quit command mode with success !\r\n");
 
-	char *dataToSend = malloc(sizeof(char)*(BUFFER_LEN_MAX+1));
-	int sizeToSend = snprintf(dataToSend, BUFFER_LEN_MAX, "Data send by server module");
-	if(CODE_RETURN_SUCCESS != rn4871TransparentUartSendData(&dev, dataToSend, sizeToSend)) {
-		printf("Error to transmit data through transparent uart ...\r\n");
+	printf("Now, wait streaming state !\r\n");
+	char receivedData[BUFFER_LEN_MAX+1] = "";
+	uint16_t receivedDataLen = 0;
+	while(FSM_STATE_STREAMING != rn4871GetFsmState(&module))
+	{
+		/* Wait a little :P */
+		rn4871WaitReceivedData(&module, receivedData, &receivedDataLen);
 	}
-	else {
-		printf("Success to transmit data through transparent uart !\r\n");
-	}
-	rn4871WaitReceivedData(&dev, dataToRecv, &dataToRecvLen);
-	printf("[%d] %s\r\n", dataToRecvLen, dataToRecv);
 
-	free(dataToRecv);
-	free(dataToSend);
+	char dataToSend[BUFFER_LEN_MAX+1] = "";
+	uint16_t dataToSendLen = 0;
+	uint32_t counter = 0;
+	while(FSM_STATE_STREAMING == rn4871GetFsmState(&module))
+	{
+		dataToSendLen = snprintf(dataToSend, BUFFER_LEN_MAX, "Counter:%d", counter);
+		rn4871TransparentUartSendData(&module, dataToSend, dataToSendLen);
+		rn4871WaitReceivedData(&module, receivedData, &receivedDataLen);
+		++counter;
+		sleep(4);
+	}
+
+	printf("Streaming is close !\r\n");
 	close(serialPort);
-	fclose(log_file);
+	//fclose(log_file);
 	printf("Serial port close with success !\r\n");
 	return 0;
 }
